3ed7b3b5cefd29b6b487b5dcb932475c2b4b36a7
fikri8484 <fikrihanif84@gmail.com>
2021-01-09 14:58:29 +0700

    first commit

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -162,7 +162,7 @@
 									<a href="/" class="link-term mercado-item-title"><i class="fa fa-home" aria-hidden="true"></i></a>
 								</li>
 								<li class="menu-item">
-									<a href="about-us.html" class="link-term mercado-item-title">About Us</a>
+									<a href="#" class="link-term mercado-item-title">About Us</a>
 								</li>
 								<li class="menu-item">
 									<a href="/shop" class="link-term mercado-item-title">Shop</a>
