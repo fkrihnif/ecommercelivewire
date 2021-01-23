09a9c3bffc6b4bc60e7faf679de90485b07ef08a
fikri8484 <fikrihanif84@gmail.com>
2021-01-23 14:41:16 +0700

    update cart and serach

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -1,6 +1,5 @@
 <main id="main">
     <div class="container">
-
         <!--MAIN SLIDE-->
         <div class="wrap-main-slide">
             <div class="slide-carousel owl-carousel style-nav-1" data-items="1" data-loop="1" data-nav="true" data-dots="false">
